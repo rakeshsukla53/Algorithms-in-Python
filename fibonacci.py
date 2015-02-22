fa2806885e4d9a53f8f4d8780763e7951aef0dee
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-02-21 20:20:12 -0500

    Dynamic Programming

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,17 @@
 __author__ = 'rakesh'
 
+a = []
+
+#fibonacci using the recursion method
+def Fibo(n):
+    a.append(0)
+    a.append(1)
+
+    for i in range(2, n):
+        a.append(a[i-1] + a[i-2])
+
+    return a
+
+print Fibo(100)
+
+    
